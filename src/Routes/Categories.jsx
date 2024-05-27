e70a5e315503a3cf5ca02ddd9ee74447b17f7497
Andy <andy@lancito01.dev>
2024-05-27 20:22:46 -0300

    agregado "modelo" a categorias

--- src/Routes/Categories.jsx
+++ src/Routes/Categories.jsx
@@ -8,17 +8,31 @@ const CreateCategories = () => {
     const token = getToken();
 
     const [brands, setBrands] = useState([])
-    // const [models, setModels] = useState([])
+    const [models, setModels] = useState([])
     const [types, setTypes] = useState([])
     useEffect(() => {
-        axios.get("http://localhost:8080/brand/all").then((res) => {
+        axios.get("http://localhost:8080/brand/all", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + token,
+            }
+        }).then((res) => {
             setBrands(res.data);
         });
-        //? seguramente se elimine esto
-        // axios.get('http://localhost:8080/model/all').then(res => {
-        //     setModels(res.data)
-        // })
-        axios.get('http://localhost:8080/type/all').then(res => {
+        axios.get("http://localhost:8080/model/all", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + token,
+            }
+        }).then((res) => {
+            setModels(res.data);
+        });
+        axios.get('http://localhost:8080/type/all', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + token,
+            }
+        }).then(res => {
             setTypes(res.data)
         })
     }, [])
@@ -33,6 +47,10 @@ const CreateCategories = () => {
                 break;
             case 'type':
                 exists = types.filter(type => type.name === value);
+                if (exists.length > 0) return alert('Ya existe esa categoría');
+                break;
+            case 'model':
+                exists = models.filter(model => model.name === value);
                 if (exists.length > 0) return alert('Ya existe esa categoría');
                 break;
             default:
@@ -51,6 +69,9 @@ const CreateCategories = () => {
                     break;
                 case 'type':
                     setTypes([...types, res.data])
+                    break;
+                case 'model':
+                    setModels([...models, res.data])
                     break;
                 default:
                     break;
@@ -81,6 +102,26 @@ const CreateCategories = () => {
         })
     }
 
+    function submitDeleteForm(e) {
+        e.preventDefault()
+
+        if (confirm("¿Estás seguro de que deseas eliminar esta categoría?") === false) return console.log('cancelado')
+
+        // todo: agregar axios.delete(), eliminar esta linea
+        return alert("Función no implementada")
+
+        console.log(e.target[0])
+        const catLabel = e.target[0].value.toLowerCase();
+        // check for form's class to know which category to delete
+        if (e.target.className === 'delete-brand-form') {
+            const brandObj = brands.find(brand => brand.name.toLowerCase() === catLabel)
+            deleteCategory('brand', brandObj.id)
+        } else if (e.target.className === 'delete-type-form') {
+            const typeObj = types.find(type => type.name.toLowerCase() === catLabel)
+            deleteCategory('type', typeObj.id)
+        }
+    }
+
     return (
         <div className='create__categories__container'>
             <h2 className='title__admin'>Administración</h2>
@@ -98,8 +139,11 @@ const CreateCategories = () => {
                         </div>
                         <div>
                             <h4>Modelo: </h4>
-                            <input type="text" />
-                            <button>Crear</button>
+                            <input type="text" id='model-input' />
+                            <button onClick={() => {
+                                const model = document.getElementById('model-input').value
+                                createCategory('model', model)
+                            }}>Crear</button>
                         </div>
                         <div>
                             <h4>Tipo: </h4>
@@ -115,25 +159,23 @@ const CreateCategories = () => {
                         <h3>Eliminar categorías</h3>
                         <div>
                             <h4>Marca: </h4>
-                            <select>
-                                {brands?.map((type, index) => {
-                                    return (
-                                        <option key={index}>{type}</option>
-                                    )
-                                })}
-                            </select>
-                            <button onClick={() => {
-                                const brandLabel = document.getElementById('delete-brand-select').value.toLowercase().strip();
-                                const brandObj = brands.find(brand => brand.name.toLowerCase().strip() === brandLabel);
-                                deleteCategory('brand', brandObj.id);
-                            }}>Borrar</button>
+                            <form onSubmit={submitDeleteForm} className='delete-brand-form'>
+                                <select>
+                                    {brands?.map((type, index) => {
+                                        return (
+                                            <option key={index}>{type.name}</option>
+                                        )
+                                    })}
+                                </select>
+                                <button>Borrar</button>
+                            </form>
                         </div>
                         <div>
                             <h4>Modelo: </h4>
                             <select>
-                                {["modelo1", "modelo2", "modelo3", "modelo4"].map((type, index) => {
+                                {models?.map((type, index) => {
                                     return (
-                                        <option key={index}>{type}</option>
+                                        <option key={index}>{type.name}</option>
                                     )
                                 })}
                             </select>
@@ -141,18 +183,16 @@ const CreateCategories = () => {
                         </div>
                         <div>
                             <h4>Tipo: </h4>
-                            <select id='delete-type-select'>
-                                {types?.map((type, index) => {
-                                    return (
-                                        <option key={index} value={type}>{type}</option>
-                                    )
-                                })}
-                            </select>
-                            <button onClick={() => {
-                                const typeLabel = document.getElementById('delete-type-select').value.toLowercase().strip()
-                                const typeObj = types.find(type => type.name.toLowerCase() === typeLabel)
-                                deleteCategory('type', typeObj.id)
-                            }}>Borrar</button>
+                            <form onSubmit={submitDeleteForm} className='delete-type-form'>
+                                <select>
+                                    {types?.map((type, index) => {
+                                        return (
+                                            <option key={index}>{type.name}</option>
+                                        )
+                                    })}
+                                </select>
+                                <button>Borrar</button>
+                            </form>
                         </div>
                     </div>
                 </div>
