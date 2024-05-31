cd4be19bb4b41f8d5905c09557243024b8de8f0c
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2024-05-31 20:09:47 -0300

    Cambios en vista Register

--- src/Routes/Register.jsx
+++ src/Routes/Register.jsx
@@ -1,16 +1,48 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useStorage } from "../Context/StorageContext";
 import { useNavigate } from "react-router-dom";
 
-
 const Register = () => {
   const { registerAPICall } = useStorage();
-  const [user, setUser] = useState({ name: "", username: "", email: "", password: "", lastName: "" });
-  const [error, setError] = useState(false);
-  const { name, username, email, password, lastName } = user;
+  const [user, setUser] = useState({
+    name: "",
+    usernameOrEmail: "",
+    email: "",
+    password: "",
+    lastName: "",
+  });
+  const { name, usernameOrEmail, email, password, lastName } = user;
   const navigator = useNavigate();
+
+  const [validateUserEmail, setValidateUserEmail] = useState(false);
+  const [validateName, setValidateName] = useState(false);
+
+  const [validateUsername, setValidateUsername] = useState(false);
+  const [validateUserLastName, setValidateUserLastName] = useState(false);
+
+  const [validatePassword, setValidatePassword] = useState(false);
+  const [boton, habilitarBoton] = useState(true);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+      try {
+        registerAPICall(user)
+          .then(async (res) => {
+            console.log(res)
+            succesMessage();
+            setTimeout(() => {
+              navigator("/login");
+             }, 2499);
+          })
+          .catch((err) => {
+            errorMessage(err.response.data.message);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+  };
 
   const succesMessage = () =>
     toast.success("Registro exitoso!!!", {
@@ -23,48 +55,54 @@ const Register = () => {
       progress: undefined,
     });
 
-    const errorMessage = () =>
-      toast.error("Verifique los campos!", {
-        position: "top-right",
-        autoClose: 2500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (validateEmail(email) && password.length >= 6 && name.length > 3) {
+  const errorMessage = (message) =>
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 2500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
 
-      try {
-        setError(false);
-        setUser({ email: "", password: "", name: "", lastName: "", username: "" });
-        registerAPICall(user).then(async (res) => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        })
-        succesMessage();
-        setTimeout(() => {
-          navigator("/login");
-          window.location.reload();
-          }, 2499);
-      } catch (error) {
-       console.log(error);;
-      }
+  useEffect(() => {
+    if (usernameOrEmail.length !== 0) {
+      setValidateUsername(usernameOrEmail.length > 3);
     }
-    else{
-        errorMessage();
+  }, [usernameOrEmail]);
+
+  useEffect(() => {
+    if (password.length !== 0) {
+      setValidatePassword(password.length > 4);
     }
-  };
+  }, [password]);
 
+  useEffect(() => {
+    if (email.length !== 0) {
+      setValidateUserEmail(email.includes(".") && email.includes("@"));
+    }
+  }, [email]);
 
-  const validateEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+.com$/;
-    return emailRegex.test(email);
-  };
+  useEffect(() => {
+    if (lastName.length !== 0) {
+      setValidateUserLastName(lastName.length > 3);
+    }
+  }, [lastName]);
+
+  useEffect(() => {
+    if (name.length !== 0) {
+      setValidateName(name.length > 3);
+    }
+  }, [name]);
+
+  useEffect(() => {
+    if (!!validatePassword && !!validateUsername && !!validateUserEmail) {
+      habilitarBoton(false);
+    } else {
+      habilitarBoton(true);
+    }
+  }, [validatePassword, validateUsername, validateUserEmail]);
 
   return (
     <div className="flex-container centered">
@@ -72,42 +110,88 @@ const Register = () => {
         <div className="card ">
           <p className="title-form">Crear Cuenta</p>
           <div className="inputContainer">
-            <input value={email} placeholder="Ingrese su email" name="email" type="text"
-              onChange={(e) => setUser({ ...user, email: e.target.value })} />
+            <input
+              value={name}
+              placeholder="Ingrese su nombre"
+              name="name"
+              type="text"
+              onChange={(e) => setUser({ ...user, name: e.target.value })}/>
+          
+          <span
+            id="comment-register"
+            className={validateName ? " error" : " visible error"}>
+            Este campo debe contener mas de 3 caracteres
+          </span>
           </div>
           <div className="inputContainer">
-            <input value={name} placeholder="Ingrese su nombre" name="name" type="text"
-              onChange={(e) => setUser({ ...user, name: e.target.value })} />
+            <input
+              value={email}
+              placeholder="Ingrese su email"
+              name="email"
+              type="text"
+              onChange={(e) => setUser({ ...user, email: e.target.value })}/>
+            <span
+              id="comment-register"
+              className={validateUserEmail ? " error" : " visible error"}>
+              Este campo debe contener un formato correcto
+            </span>
           </div>
           <div className="inputContainer">
-            <input value={username} placeholder="Ingrese su nombre de usuario" name="username" type="text"
-              onChange={(e) => setUser({ ...user, username: e.target.value })} />
+            <input
+              value={usernameOrEmail}
+              placeholder="Ingrese su nombre de usuario"
+              name="usernameOrEmail"
+              type="text"
+              onChange={(e) =>
+                setUser({ ...user, usernameOrEmail: e.target.value })}/>   
+                <span id="comment-register"
+                     className={validateUsername ? " error" : " visible error"}>
+                     Este campo debe contener mas de 3 caracteres
+                </span>
+            </div>
+          <div className="inputContainer">
+            <input
+              value={lastName}
+              placeholder="Ingrese su apellido"
+              name="lastName"
+              type="text"
+              onChange={(e) => setUser({ ...user, lastName: e.target.value })}/>   
+            <span
+            id="comment-register"
+            className={validateUserLastName ? " error" : " visible error"}>
+            Este campo debe contener mas de 3 caracteres
+          </span>
           </div>
           <div className="inputContainer">
-            <input value={lastName} placeholder="Ingrese su apellido" name="lastName" type="text"
-              onChange={(e) => setUser({ ...user, lastName: e.target.value })} />
+            <input
+              value={password}
+              placeholder="Ingrese su contraseña"
+              name="password"
+              type="password"
+              onChange={(e) => setUser({ ...user, password: e.target.value })} /> 
+            <span
+            id="comment-register"
+            className={validatePassword ? " error" : " visible error"}>
+            Este campo debe contener mas de 4 caracteres
+          </span>
           </div>
-          <div className="inputContainer">
-            <input value={password} placeholder="Ingrese su contraseña" name="password" type="password"
-              onChange={(e) => setUser({ ...user, password: e.target.value })} />
-          </div>
-
           <div className="inputContainer terminos__condiciones">
             <label htmlFor="aceptar">
               Acepto los terminos y condiciones de ArrancAR
             </label>
-            <input className="checkbox" id="checkbox" value={false} name="aceptar" type="checkbox" />
+            <input
+              className="checkbox"
+              id="checkbox"
+              value={false}
+              name="aceptar"
+              type="checkbox"/>
           </div>
-          <button className="btn-login">
+          <button className="btn" 
+          disabled={boton}>
             Enviar Datos
           </button>
           <ToastContainer />
         </div>
-        {error && (
-          <h3 className={error ? "visible error" : "error"}>
-            Por favor, verifique los campos ingresados
-          </h3>
-        )}
       </form>
     </div>
   );
