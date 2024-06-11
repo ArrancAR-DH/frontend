f9bcd631fd56280e3895616aa2a58171a51cf460
andy <andy@lancito01.dev>
2024-06-10 22:57:24 -0700

    - agregada informacion legal - arreglada Card de vehiculo:     al apretar en alguna parte de la card que no sea el titulo, el usuario no era enviado al detalle.     ahora:     al apretar en cualquier parte de la card (excepto en el boton de like) lo lleva al usuario al detalle

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,40 +3,39 @@ import utils from "../functions/utils.js";
 import { FaHeart } from "react-icons/fa";
 import { useContextGlobal } from "../Context/GlobalContext.jsx";
 
-const Card = ({car}) => {
+const Card = ({ car }) => {
   const { state, likeVehicle, dislikeVehicle, giveLike } = useContextGlobal();
-  const userLike = giveLike();  
+  const userLike = giveLike();
   const isLiked = state.likes.includes(car.idVehicle);
 
-  const handleLike = async () => {
+  const handleLike = async (e) => {
+    e.preventDefault();
     const isAlreadyLiked = state.likes.includes(car.idVehicle);
     if (isAlreadyLiked) {
       await dislikeVehicle(car.idVehicle);
-      alert("Este vehículo ha sido eliminado de tu lista de favoritos.");
+      // alert("Este vehículo ha sido eliminado de tu lista de favoritos.");
     } else {
       await likeVehicle(car.idVehicle);
-      alert("El vehículo se agregó satisfactoriamente a tu lista de favoritos.");
+      // alert("El vehículo se agregó satisfactoriamente a tu lista de favoritos.");
     }
   };
 
   return (
-    <>
+    <Link to={`/cars/${car.idVehicle}`}>
       <div className="car__card" id={car.idVehicle}>
         <img src={car.imgUrls?.[0]?.url} alt={"main-image"} />
         <div className="card__car__information ">
           <h4>{car.brand.name}</h4>
-          <Link to={`/cars/${car.idVehicle}`}>
-            <p>{car.model.name}</p>
-            <p>${utils.convertirPrecioIntAPesosStr(car.price)} ARS</p>
-          </Link>
-          {userLike? (
-              <button className={`container__heart ${isLiked ? "liked" : ""}`} onClick={handleLike}>
-                  <FaHeart className="heart" />
-              </button>
-                ): ("")}
+          <p>{car.model.name}</p>
+          <p>${utils.convertirPrecioIntAPesosStr(car.price)} ARS</p>
+          {userLike ? (
+            <button className={`container__heart ${isLiked ? "liked" : ""}`} onClick={handleLike}>
+              <FaHeart className="heart" />
+            </button>
+          ) : ("")}
         </div>
       </div>
-    </>
+    </Link>
   );
 };
 
