b766ce5998772d645e0837b16afaa867a83b72f9
andy <andy@lancito01.dev>
2024-06-11 23:01:31 -0700

    cambiado fondo de overlay de edición de vehículos para que sea un poco mas oscuro (0.7 -> 0.85)

--- src/Components/EditVehicleOverlay.jsx
+++ src/Components/EditVehicleOverlay.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useContextGlobal } from "../Context/GlobalContext";
 
-
 const URL = import.meta.env.VITE_BACKEND_URL;
 
 const EditVehicleOverlay = ({ vehicle, setVehicleBeingEdited, setCars }) => {
