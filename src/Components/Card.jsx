f302ed8f9d8b05f9012971b9c7cc43588110b931
andy <andy@lancito01.dev>
2024-06-12 23:45:20 -0700

    - arreglada + cambiada vista Card - arreglado bug: al buscar autos, todos aparecian likeados/sin likear

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,39 +4,43 @@ import { FaHeart } from "react-icons/fa";
 import { useContextGlobal } from "../Context/GlobalContext.jsx";
 
 const Card = ({ car }) => {
-  const { state, likeVehicle, dislikeVehicle, giveLike } = useContextGlobal();
-  const userLike = giveLike();
-  const isLiked = state.likes.includes(car.idVehicle);
+    const { state, likeVehicle, dislikeVehicle, giveLike } = useContextGlobal();
+    const userLike = giveLike();
+    const isLiked = state.likes.includes(car.idVehicle);
 
-  const handleLike = async (e) => {
-    e.preventDefault();
-    const isAlreadyLiked = state.likes.includes(car.idVehicle);
-    if (isAlreadyLiked) {
-      await dislikeVehicle(car.idVehicle);
-      // alert("Este vehículo ha sido eliminado de tu lista de favoritos.");
-    } else {
-      await likeVehicle(car.idVehicle);
-      // alert("El vehículo se agregó satisfactoriamente a tu lista de favoritos.");
-    }
-  };
+    const handleLike = async (e) => {
+        e.preventDefault();
+        const isAlreadyLiked = state.likes.includes(car.idVehicle);
+        if (isAlreadyLiked) {
+            await dislikeVehicle(car.idVehicle);
+            // alert("Este vehículo ha sido eliminado de tu lista de favoritos.");
+        } else {
+            await likeVehicle(car.idVehicle);
+            // alert("El vehículo se agregó satisfactoriamente a tu lista de favoritos.");
+        }
+    };
 
-  return (
-    <Link to={`/cars/${car.idVehicle}`}>
-      <div className="car__card" id={car.idVehicle}>
-        <img src={car.imgUrls?.[0]?.url} alt={"main-image"} />
-        <div className="card__car__information ">
-          <h4>{car.brand.name}</h4>
-          <p>{car.model.name}</p>
-          <p>${utils.convertirPrecioIntAPesosStr(car.price)} ARS</p>
-          {userLike ? (
-            <button className={`container__heart ${isLiked ? "liked" : ""}`} onClick={handleLike}>
-              <FaHeart className="heart" />
-            </button>
-          ) : ("")}
-        </div>
-      </div>
-    </Link>
-  );
+    return (
+        <Link to={`/cars/${car.idVehicle}`}>
+            <div className="car__card" id={car.idVehicle}>
+                <div className="car__image">
+                    <img src={car.imgUrls?.[0]?.url} alt={"main-image"} />
+                    {userLike ? (
+                        <button className={`container__heart ${isLiked ? "liked" : ""}`} onClick={handleLike}>
+                            <FaHeart className="heart" />
+                        </button>
+                    ) : ("")}
+                </div>
+                <div className="card__car__information">
+                    <div className="name">
+                        <h4>{car.brand.name}</h4>
+                        <p>{car.model.name}</p>
+                    </div>
+                    <p>${utils.convertirPrecioIntAPesosStr(car.price)} ARS</p>
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default Card;
